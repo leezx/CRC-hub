68359e9a3ba70e1395f7ce6fcebe0bcc278d07ca
Zstar <zxlee@connect.hku.hk>
2023-02-08 00:24:28 -0500

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,4 @@
+import os
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information
@@ -38,6 +39,11 @@ intersphinx_mapping = {
 }
 intersphinx_disabled_domains = ['std']
 
+# List of arguments to be passed to the kernel that executes the notebooks:
+nbsphinx_execute_arguments = [
+    "--InlineBackend.figure_formats={'svg', 'pdf'}",
+]
+
 templates_path = ['_templates']
 
 # -- Options for HTML output
@@ -47,16 +53,16 @@ html_logo = "CRChub.png"
 html_static_path = ['_static'] 
 sphinx_rtd_size_width = "120%"
 
-html_css_files = ["custom.css"]
+# html_css_files = ["custom.css"]
 # html_style = 'custom.css'
 
-# for sphinx_gallery
-sphinx_gallery_conf = {
-    'thumbnail_size': (150, 150),
-    'line_numbers': True,
-    'default_thumb_file': 'CRChub.png',
-    'nested_sections' : False,
-}
+# # for sphinx_gallery
+# sphinx_gallery_conf = {
+#     'thumbnail_size': (150, 150),
+#     'line_numbers': True,
+#     'default_thumb_file': 'CRChub.png',
+#     'nested_sections' : False,
+# }
 
 logo_only = True
 display_version = False
@@ -75,3 +81,56 @@ html_show_copyright = True
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+# This is processed by Jinja2 and inserted before each notebook
+nbsphinx_prolog = r"""
+{% set docname = 'doc/' + env.doc2path(env.docname, base=None) %}
+.. raw:: html
+    <div class="admonition note">
+      This page was generated from
+      <a class="reference external" href="https://github.com/spatialaudio/nbsphinx/blob/{{ env.config.release|e }}/{{ docname|e }}">{{ docname|e }}</a>.
+      Interactive online version:
+      <span style="white-space: nowrap;"><a href="https://mybinder.org/v2/gh/spatialaudio/nbsphinx/{{ env.config.release|e }}?filepath={{ docname|e }}"><img alt="Binder badge" src="https://mybinder.org/badge_logo.svg" style="vertical-align:text-bottom"></a>.</span>
+      <script>
+        if (document.location.host) {
+          let nbviewer_link = document.createElement('a');
+          nbviewer_link.setAttribute('href',
+            'https://nbviewer.org/url' +
+            (window.location.protocol == 'https:' ? 's/' : '/') +
+            window.location.host +
+            window.location.pathname.slice(0, -4) +
+            'ipynb');
+          nbviewer_link.innerHTML = 'View in <em>nbviewer</em>';
+          nbviewer_link.classList.add('reference');
+          nbviewer_link.classList.add('external');
+          document.currentScript.replaceWith(nbviewer_link, '.');
+        }
+      </script>
+    </div>
+.. raw:: latex
+    \nbsphinxstartnotebook{\scriptsize\noindent\strut
+    \textcolor{gray}{The following section was generated from
+    \sphinxcode{\sphinxupquote{\strut {{ docname | escape_latex }}}} \dotfill}}
+"""
+
+# This is processed by Jinja2 and inserted after each notebook
+nbsphinx_epilog = r"""
+{% set docname = 'doc/' + env.doc2path(env.docname, base=None) %}
+.. raw:: latex
+    \nbsphinxstopnotebook{\scriptsize\noindent\strut
+    \textcolor{gray}{\dotfill\ \sphinxcode{\sphinxupquote{\strut
+    {{ docname | escape_latex }}}} ends here.}}
+"""
+
+mathjax3_config = {
+    'tex': {'tags': 'ams', 'useLabelIds': True},
+}
+
+bibtex_bibfiles = ['references.bib']
+
+# Support for notebook formats other than .ipynb
+nbsphinx_custom_formats = {
+    '.pct.py': ['jupytext.reads', {'fmt': 'py:percent'}],
+    '.md': ['jupytext.reads', {'fmt': 'Rmd'}],
+}
+
