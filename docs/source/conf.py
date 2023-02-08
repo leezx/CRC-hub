938389df01266138be97ce429ef00f5407b0f632
Zhixin Eason Li <zxlee@connect.hku.hk>
2023-02-07 19:11:52 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,7 @@ html_logo = "CRChub.png"
 html_static_path = ['_static'] 
 sphinx_rtd_size_width = "100%"
 
-html_style = 'css/custom.css'
+html_css_files = 'css/custom.css'
 
 # for sphinx_gallery
 sphinx_gallery_conf = {
