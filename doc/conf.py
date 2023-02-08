60401829c51736b698063c575a7b629403de0000
Zstar <zxlee@connect.hku.hk>
2023-02-08 02:23:44 -0500

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -42,54 +42,54 @@ intersphinx_mapping = {
 #     'orphan': '_static/favicon.svg',
 # }
 
-# # This is processed by Jinja2 and inserted before each notebook
-# nbsphinx_prolog = r"""
-# {% set docname = 'doc/' + env.doc2path(env.docname, base=None) %}
+# This is processed by Jinja2 and inserted before each notebook
+nbsphinx_prolog = r"""
+{% set docname = 'doc/' + env.doc2path(env.docname, base=None) %}
 
-# .. raw:: html
+.. raw:: html
 
-#     <div class="admonition note">
-#       This page was generated from
-#       <a class="reference external" href="https://github.com/spatialaudio/nbsphinx/blob/{{ env.config.release|e }}/{{ docname|e }}">{{ docname|e }}</a>.
-#       Interactive online version:
-#       <span style="white-space: nowrap;"><a href="https://mybinder.org/v2/gh/spatialaudio/nbsphinx/{{ env.config.release|e }}?filepath={{ docname|e }}"><img alt="Binder badge" src="https://mybinder.org/badge_logo.svg" style="vertical-align:text-bottom"></a>.</span>
-#       <script>
-#         if (document.location.host) {
-#           let nbviewer_link = document.createElement('a');
-#           nbviewer_link.setAttribute('href',
-#             'https://nbviewer.org/url' +
-#             (window.location.protocol == 'https:' ? 's/' : '/') +
-#             window.location.host +
-#             window.location.pathname.slice(0, -4) +
-#             'ipynb');
-#           nbviewer_link.innerHTML = 'View in <em>nbviewer</em>';
-#           nbviewer_link.classList.add('reference');
-#           nbviewer_link.classList.add('external');
-#           document.currentScript.replaceWith(nbviewer_link, '.');
-#         }
-#       </script>
-#     </div>
+    <div class="admonition note">
+      This page was generated from
+      <a class="reference external" href="https://github.com/spatialaudio/nbsphinx/blob/{{ env.config.release|e }}/{{ docname|e }}">{{ docname|e }}</a>.
+      Interactive online version:
+      <span style="white-space: nowrap;"><a href="https://mybinder.org/v2/gh/spatialaudio/nbsphinx/{{ env.config.release|e }}?filepath={{ docname|e }}"><img alt="Binder badge" src="https://mybinder.org/badge_logo.svg" style="vertical-align:text-bottom"></a>.</span>
+      <script>
+        if (document.location.host) {
+          let nbviewer_link = document.createElement('a');
+          nbviewer_link.setAttribute('href',
+            'https://nbviewer.org/url' +
+            (window.location.protocol == 'https:' ? 's/' : '/') +
+            window.location.host +
+            window.location.pathname.slice(0, -4) +
+            'ipynb');
+          nbviewer_link.innerHTML = 'View in <em>nbviewer</em>';
+          nbviewer_link.classList.add('reference');
+          nbviewer_link.classList.add('external');
+          document.currentScript.replaceWith(nbviewer_link, '.');
+        }
+      </script>
+    </div>
 
-# .. raw:: latex
+.. raw:: latex
 
-#     \nbsphinxstartnotebook{\scriptsize\noindent\strut
-#     \textcolor{gray}{The following section was generated from
-#     \sphinxcode{\sphinxupquote{\strut {{ docname | escape_latex }}}} \dotfill}}
-# """
+    \nbsphinxstartnotebook{\scriptsize\noindent\strut
+    \textcolor{gray}{The following section was generated from
+    \sphinxcode{\sphinxupquote{\strut {{ docname | escape_latex }}}} \dotfill}}
+"""
 
-# # This is processed by Jinja2 and inserted after each notebook
-# nbsphinx_epilog = r"""
-# {% set docname = 'doc/' + env.doc2path(env.docname, base=None) %}
-# .. raw:: latex
+# This is processed by Jinja2 and inserted after each notebook
+nbsphinx_epilog = r"""
+{% set docname = 'doc/' + env.doc2path(env.docname, base=None) %}
+.. raw:: latex
 
-#     \nbsphinxstopnotebook{\scriptsize\noindent\strut
-#     \textcolor{gray}{\dotfill\ \sphinxcode{\sphinxupquote{\strut
-#     {{ docname | escape_latex }}}} ends here.}}
-# """
+    \nbsphinxstopnotebook{\scriptsize\noindent\strut
+    \textcolor{gray}{\dotfill\ \sphinxcode{\sphinxupquote{\strut
+    {{ docname | escape_latex }}}} ends here.}}
+"""
 
-# mathjax3_config = {
-#     'tex': {'tags': 'ams', 'useLabelIds': True},
-# }
+mathjax3_config = {
+    'tex': {'tags': 'ams', 'useLabelIds': True},
+}
 
 # bibtex_bibfiles = ['references.bib']
 
